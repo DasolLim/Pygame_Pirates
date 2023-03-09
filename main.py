0fb2cd31c04753cf8578574f4088453d2f37430c
Christopher Higgins <113563713+chiggi24@users.noreply.github.com>
2023-03-09 11:43:40 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+pygame.init()
+
+# Initialize the game
+bg_img = pygame.image.load('backgroundimage.png')
+bg_img = pygame.transform.scale(bg_img,(1280,780))
+bg_rect = bg_img.get_rect()
+
+screen = pygame.display.set_mode((bg_rect.width, bg_rect.height))
+screen_rect = screen.get_rect()
+
+
+def render():
+    screen.blit(bg_img,bg_rect)
+    pygame.display.flip()
+
+
+render()
+running = True
+FPS = 60
+clock = pygame.time.Clock()
+# gameloop
+while running:
+    clock.tick(FPS)
+    #event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #render
+    render()
+
+
+pygame.quit()
